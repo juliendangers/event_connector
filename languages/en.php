64247045ff885220ec23ea7e6896546e9d854935
Sem <sembrestels@riseup.net>
2012-05-14 12:17:23 +0200

    Retabbed language files.

--- languages/en.php
+++ languages/en.php
@@ -1,33 +1,31 @@
 <?php
 
-	$english = array(
-		'event_connector:menu:one' => 'Export to iCal',
-		'event_connector:menu:all' => 'Export all events to iCal',
-		
-		'event_connector:upload:file' => "iCal (.ics) file",
-		'event_connector:upload' => "Import event",
-		
-		'event_connector:events:saved' => "Events successfully added",
-		'event_connector:event:saved' => "Event successfully added",
-		
-		'event_connector:import:title' => 'Import iCal event(s) to your calendar',
-		'event_connector:import' => "Import event(s) from iCal",
-		
-		'event_connector:timezone' => "Chose your timezone",
-		
-		'event_connector:no_event' => 'No events found',
-		'event_connector:activation:failed' => "You must activate the event_calendar plugin",
-		'event_connector:enabled' => "You must activate the event_connector plugin",
-		
-		'event_connector:no_such_event' => "No corresponding event",
-		'event_connector:error:notactivated' => "You have to activate event_connector",
-		'event_connector:error:group_calendar' => "Event calendar is not activated for this group",
-		'event_connector:error:noevent' => 'There is no event in the file',
-		'event_connector:error:failed' => "An error occured, we were unable to import your event(s)",
-		'event_connector:error:error_upload' => "Unable to upload your file",
-		'event_connector:error:format' => "Wrong file format",
-	);
-					
-	add_translation("en",$english);
-
-?>
+$english = array(
+	'event_connector:menu:one' => 'Export to iCal',
+	'event_connector:menu:all' => 'Export all events to iCal',
+	
+	'event_connector:upload:file' => "iCal (.ics) file",
+	'event_connector:upload' => "Import event",
+	
+	'event_connector:events:saved' => "Events successfully added",
+	'event_connector:event:saved' => "Event successfully added",
+	
+	'event_connector:import:title' => 'Import iCal event(s) to your calendar',
+	'event_connector:import' => "Import event(s) from iCal",
+	
+	'event_connector:timezone' => "Chose your timezone",
+	
+	'event_connector:no_event' => 'No events found',
+	'event_connector:activation:failed' => "You must activate the event_calendar plugin",
+	'event_connector:enabled' => "You must activate the event_connector plugin",
+	
+	'event_connector:no_such_event' => "No corresponding event",
+	'event_connector:error:notactivated' => "You have to activate event_connector",
+	'event_connector:error:group_calendar' => "Event calendar is not activated for this group",
+	'event_connector:error:noevent' => 'There is no event in the file',
+	'event_connector:error:failed' => "An error occured, we were unable to import your event(s)",
+	'event_connector:error:error_upload' => "Unable to upload your file",
+	'event_connector:error:format' => "Wrong file format",
+);
+				
+add_translation("en", $english);
